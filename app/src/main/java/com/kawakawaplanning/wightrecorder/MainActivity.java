a011740215c7ad615f99c5b1d2f61ad17e1af06a
kawakawaritsuki.dev@gmail.com <kawakawakun>
2015-03-25 10:19:05 +0900

    Tab実装

--- app/src/main/java/com/kawakawaplanning/wightrecorder/MainActivity.java
+++ app/src/main/java/com/kawakawaplanning/wightrecorder/MainActivity.java
@@ -1,19 +1,53 @@
 package com.kawakawaplanning.wightrecorder;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity implements FragmentTabHost.OnTabChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // FragmentTabHost を取得する
+        FragmentTabHost tabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+        tabHost.setup(this, getSupportFragmentManager(), R.id.container);
+
+        TabHost.TabSpec tabSpec1, tabSpec2, tabSpec3;
+
+        // TabSpec を生成する
+        tabSpec1 = tabHost.newTabSpec("tab1");
+        tabSpec1.setIndicator("tab1");
+        // TabHost に追加
+        tabHost.addTab(tabSpec1, SampleFragment.class, null);
+
+        // TabSpec を生成する
+        tabSpec2 = tabHost.newTabSpec("tab2");
+        tabSpec2.setIndicator("tab2");
+        // TabHost に追加
+        tabHost.addTab(tabSpec2, SampleFragment.class, null);
+
+        // TabSpec を生成する
+        tabSpec3 = tabHost.newTabSpec("tab3");
+        tabSpec3.setIndicator("tab3");
+        // TabHost に追加
+        tabHost.addTab(tabSpec3, SampleFragment.class, null);
+
+        // リスナー登録
+        tabHost.setOnTabChangedListener(this);
     }
 
+    @Override
+    public void onTabChanged(String tabId) {
+        Log.d("onTabChanged", "tabId: " + tabId);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
